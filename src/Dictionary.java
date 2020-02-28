77dcd869b76350f5c2858154e3f23c5b15bcb6dc
GittyDitty <andrew.d98275@icloud.com>
2020-02-27 21:50:43 -0800

    Made some changes that need to be looked at tomorrow.

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,8 +1,8 @@
 public interface Dictionary{
     boolean isEmpty();
     boolean add(int key, int value);
-    boolean remove(int key);
-    boolean get(int key, int value);
-    boolean contains(int key);
-    void traverse();
+    //boolean remove(int key);
+    //boolean get(int key, int value);
+    //boolean contains(int key);
+    //void traverse();
 }
--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -17,16 +17,26 @@ public class MyMiniSearchEngine {
 
     // each item in the List is considered a document.
     // assume documents only contain alphabetical words separated by white spaces.
+    //this should assign each keyword or search to it's own unique index maybe use ArrayDictionary for this?
     private void index(List<String> texts) {
         //homework
+        ArrayDictionary test = new ArrayDictionary(texts.size());
+        test.add(1, texts.size()); // Maybe implement a math.rand for the indexes?
+
         List<String> text_lower; // the new list of strings shoudld take the texts and place them to lower value
     }
 
     // search(key) return all the document ids where the given key phrase appears.
     // key phrase can have one or two words in English alphabetic characters.
     // return an empty list if search() finds no match in all documents.
+    // DON'T USE STRING COMPARISON TO SCAN EACH DOC TO DO SEARCHES UNLESS YOU WANT TO LOSE 50 PTS!
+    // Remember that this can search either a single word such as "Hello" or a phrase >=2 words such as "Hello world"
     public List<Integer> search(String keyPhrase) {
-        // homework
+        // homework\
+        if(keyPhrase == null){
+            return new ArrayList<>(); // if they don't search anything then nothing will be returned except for an empty arrayList
+        }
+
         return new ArrayList<>(); // place holder
     }
 }
