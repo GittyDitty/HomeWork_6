342d4667276ab06f472f39278284d29cde1c3f62
GittyDitty <andrew.d98275@icloud.com>
2020-03-05 13:33:03 -0800

    Final Implementation of HW#6

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.*;
+import java.util.HashMap;
+
+/*
+CSS143B homework 6: Search Engine
+by [Andrew Dakan]
+ */
 
 public class MyMiniSearchEngine {
     // default solution. OK to change.
@@ -20,11 +25,10 @@ public class MyMiniSearchEngine {
     // assume documents only contain alphabetical words separated by white spaces.
     // this should assign each keyword or search to it's own unique index
     private void index(List<String> texts) {
-        //texts is documents!
         indexes = new HashMap<String, List<List<Integer>>>();
 
         for(int i=0;i<texts.size();i++){
-            String[] words = texts.get(i).split(" ");
+            String[] words = texts.get(i).toLowerCase().split(" ");
             for(int j=0; j<words.length;j++) {
                 String word = words[j];
                 List<List<Integer>> depth = new ArrayList<>();
@@ -42,46 +46,51 @@ public class MyMiniSearchEngine {
     // search(key) return all the document ids where the given key phrase appears.
     // key phrase can have one or two words in English alphabetic characters.
     // return an empty list if search() finds no match in all documents.
-    // DON'T USE STRING COMPARISON TO SCAN EACH DOC TO DO SEARCHES UNLESS YOU WANT TO LOSE 50 PTS!
-    // Remember that this can search either a single word such as "Hello" or a phrase >=2 words such as "Hello world"
-    /*
-    // Basic step by step process for this:
-        1.) if it's a two or three word search then split the searches for each word
-            make sure that the main search is to lower case value.
-     */
     //For the cases in which the keyPhrase cannot be found inside any of the documents it simply returns -1
     public List<Integer> search(String keyPhrase) {
         // homework
         //start out with single word search and make your way up from there
-
-        if (keyPhrase.contains(" ")){//multiple word case
-            ArrayList<Integer> toReturn = new ArrayList<>();
-            if(!indexes.containsKey(keyPhrase)){ //if the indexes don't contain the keyword then returns an ArrayList containing -1
-                ArrayList<Integer> bad = new ArrayList<>();
-                bad.add(-1);
-                return bad;
-            }
-            //do this part later or simply ask someone tomorrow :)
-            ArrayList<Integer> word_1 = new ArrayList<>();
-            ArrayList<Integer> word_2 = new ArrayList<>();
-
-
-            return toReturn;
-        }else{ //single word case
-            ArrayList<Integer> toReturn = new ArrayList<>(); //we add/index the values that we find from searching into this
-            if(!indexes.containsKey(keyPhrase)){
-                ArrayList<Integer> bad = new ArrayList<>();
-                bad.add(-1);
-                return bad;
-            }
-            for(int i = 0; i<indexes.size(); i++){
-                ArrayList<Integer> searcher = new ArrayList<>();
-                if(indexes.get(i).equals(keyPhrase)){
-                    searcher.add(indexes.get(i).size());
+        String[] words = keyPhrase.toLowerCase().split(" ");
+        List<Integer> list = new ArrayList<>();
+        if(words.length == 1) {//singular word
+            if(indexes.containsKey(words[0])) {
+                for (int i = 0; i < indexes.get(words[0]).size(); i++) {
+                    if (indexes.get(words[0]).get(i).size() > 0) {
+                        list.add(i);
+                    }
                 }
-             //toReturn.add(searcher.size());
+                return list;
+            }else{
+                list = new ArrayList<>();
+                list.add(-1);
+                return list;
             }
-            return toReturn;
+        }else{//multiple word
+            for(int i=0; i<words.length-1; i++){
+                if(indexes.containsKey(words[i])){
+                    if(indexes.containsKey(words[i+1])){
+                        for(int j=0; j<indexes.get(words[i]).size();j++){
+                            if((indexes.get(words[i]).get(j).size() > 0) && (indexes.get(words[i+1]).get(j).size() > 0)){
+                                for(int k=0; k<indexes.get(words[i]).get(j).size();k++){
+                                    if(indexes.get(words[i]).get(j).get(k)+1 == indexes.get(words[i+1]).get(j).get(k)){
+                                        if(!list.contains(j))
+                                        list.add(j);
+                                    }
+                                }
+                            }
+                        }
+                    }else{
+                        list = new ArrayList<>();
+                        list.add(-1);
+                        return list;
+                    }
+                }else{
+                    list = new ArrayList<>();
+                    list.add(-1);
+                    return list;
+                }
+            }
         }
+        return list;
     }
 }
